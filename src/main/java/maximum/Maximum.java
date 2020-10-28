4e6ae6323c019bd2204f6aff0040a59cd0f449d0
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-28 21:13:40 +0530

    ADDED:maximumInteger method and testcases for maximumInterger

--- src/main/java/maximum/Maximum.java
+++ src/main/java/maximum/Maximum.java
@@ -1,4 +1,14 @@
 package maximum;
 
 public class Maximum {
+    public Integer maximumInteger(Integer x,Integer y,Integer z) {
+        Integer maximum = x;
+        if(y.compareTo(maximum)>0){
+            maximum=y;
+        }
+        if(z.compareTo(maximum)>0){
+            maximum=z;
+        }
+        return maximum;
+    }
 }
--- src/test/java/com/testing/MaximumTest.java
+++ src/test/java/com/testing/MaximumTest.java
@@ -1,9 +1,34 @@
 package com.testing;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import maximum.Maximum;
 
 public class MaximumTest {
+    Maximum maximum;
 
+    @Before
+    public void setUp() throws Exception {
+        maximum=new Maximum();
+    }
+
+    @Test
+    public void givenMaximumInteger_AsAFirstParameter_ShouldReturnMaximum() {
+        Integer maximumResult=maximum.maximumInteger(5,4,3);
+        Integer expectedResult=5;
+        Assert.assertEquals(expectedResult,maximumResult);
+    }
+    @Test
+    public void givenMaximumInteger_AsSecondParameter_ShouldReturnMaximum(){
+        Integer maximumResult=maximum.maximumInteger(4,5,3);
+        Integer expectedResult=5;
+        Assert.assertEquals(expectedResult,maximumResult);
+    }
+    @Test
+    public void givenMaximumInteger_AsThirdParameter_ShouldReturnMaximum() {
+        Integer maximumResult=maximum.maximumInteger(3,4,5);
+        Integer expectedResult=5;
+        Assert.assertEquals(expectedResult,maximumResult);
+    }
 }
