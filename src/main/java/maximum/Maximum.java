761293c264afb93606472d0744ad87aae74559cb
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-29 09:51:25 +0530

    REFRACTED:all maximum method and added checkMaximum generic method

--- src/main/java/maximum/Maximum.java
+++ src/main/java/maximum/Maximum.java
@@ -1,36 +1,15 @@
 package maximum;
 
 public class Maximum {
-    public Integer maximumInteger(Integer x,Integer y,Integer z) {
-        Integer maximum = x;
-        if(y.compareTo(maximum)>0){
-            maximum=y;
-        }
-        if(z.compareTo(maximum)>0){
-            maximum=z;
-        }
-        return maximum;
-    }
 
-    public Float maximumFloat(Float x,Float y,Float z) {
-        Float maximum=x;
-        if(y.compareTo(maximum)>0){
-            maximum=y;
-        }
-        if(z.compareTo(maximum)>0){
-            maximum=z;
-        }
-        return maximum;
-    }
-
-    public String maximumString(String x, String y, String z) {
-        String maximum=x;
-        if(y.compareTo(maximum)>0){
-            maximum=y;
-        }
-        if(z.compareTo(maximum)>0){
-            maximum=z;
-        }
-        return maximum;
+    public static <T extends Comparable<T>> T checkMaximum(T x,T y,T z){
+       T maximum=x;
+       if(y.compareTo(maximum)>0){
+           maximum=y;
+       }
+       if(z.compareTo(maximum)>0){
+           maximum=z;
+       }
+       return maximum;
     }
 }
--- src/test/java/com/testing/MaximumTest.java
+++ src/test/java/com/testing/MaximumTest.java
@@ -15,54 +15,54 @@ public class MaximumTest {
 
     @Test
     public void givenMaximumInteger_AsAFirstParameter_ShouldReturnMaximumInteger() {
-        Integer maximumResult=maximum.maximumInteger(5,4,3);
+        Integer maximumResult=maximum.checkMaximum(5,4,3);
         Integer expectedResult=5;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumInteger_AsSecondParameter_ShouldReturnMaximumInterger(){
-        Integer maximumResult=maximum.maximumInteger(4,5,3);
+        Integer maximumResult=maximum.checkMaximum(4,5,3);
         Integer expectedResult=5;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumInteger_AsThirdParameter_ShouldReturnMaximumInteger() {
-        Integer maximumResult=maximum.maximumInteger(3,4,5);
+        Integer maximumResult=maximum.checkMaximum(3,4,5);
         Integer expectedResult=5;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumFloat_AsFirstParameter_ShouldReturnMaximumFloat() {
-        Float maximumResult=maximum.maximumFloat(8.8f,7.7f,6.6f);
+        Float maximumResult=maximum.checkMaximum(8.8f,7.7f,6.6f);
         Float expectedResult=8.8f;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumFloat_AsSecondParameter_ShouldReturnMaximumFloat() {
-        Float maximumResult=maximum.maximumFloat(7.7f,8.8f,6.6f);
+        Float maximumResult=maximum.checkMaximum(7.7f,8.8f,6.6f);
         Float expectedResult=8.8f;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumFloat_AsThirdParameter_ShouldReturnMaximumFloat(){
-        Float maximumResult=maximum.maximumFloat(6.6f,7.7f,8.8f);
+        Float maximumResult=maximum.checkMaximum(6.6f,7.7f,8.8f);
         Float expectedResult=8.8f;
         Assert.assertEquals(expectedResult,maximumResult);
     }
     @Test
     public void givenMaximumString_AsFirstParameter_ShouldReturnMaximumString() {
-        String maximumResult=maximum.maximumString("peach","orange","apple");
+        String maximumResult=maximum.checkMaximum("peach","orange","apple");
         Assert.assertEquals("peach",maximumResult);
     }
     @Test
     public void givenMaximumString_AsSecondParameter_ShouldReturnMaximumString() {
-        String maximumResult=maximum.maximumString("orange","peach","apple");
+        String maximumResult=maximum.checkMaximum("orange","peach","apple");
         Assert.assertEquals("peach",maximumResult);
     }
 
     @Test
-    public void givenMaximumstring_AsThirdParameter_ShouldReturnMaximumString() {
-        String maximumResult=maximum.maximumString("apple","orange","peach");
+    public void givenMaximumString_AsThirdParameter_ShouldReturnMaximumString() {
+        String maximumResult=maximum.checkMaximum("apple","orange","peach");
         Assert.assertEquals("peach",maximumResult);
     }
 }
